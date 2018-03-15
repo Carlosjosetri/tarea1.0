63312fcfd79cc4b8ddac0202def5167ad1b9f6b6
UCA <edwin.morales9b@gmail.com>
2018-03-14 19:39:10 -0600

    Proyecto Base2

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,20 @@
+
+package javaapplication2;
+
+/**
+ *
+ * @author Trigueros
+ */
+public class JavaApplication2 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Carlos José Trigueros Rivas");
+        System.out.println("Carnet: 00082217");
+        System.out.println("Facultad de Ingeniería y Arquitectura");
+        System.out.println("Carrera: Ingeniería informática");
+        
+        
+    }
+    
+}
